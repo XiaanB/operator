53cab61cde0363ba5c3037f09801e6f6bd6245c3
xiaan <xiaan_b@hotmail.com>
2022-03-04 10:46:55 +1300

    Initial commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int result = 1 + 2; //1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        int previousResult = result;
+        System.out.println("previous result = " + previousResult);
+
+        result = result - 1;
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previous result = " + previousResult);
+
+        result = result * 10;
+        System.out.println("2*10= " + result);
+
+        result = result / 5;
+        System.out.println("20/5= " + result);
+
+        result = result % 3;
+        System.out.println("4 % 3 = " + result );
+
+
+    }
+}
